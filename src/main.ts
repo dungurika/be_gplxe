6540cf3bcfdb7a4614389247d4f7641ed72c48fa
Trung Dung <dungtran@urika.me>
2023-05-13 13:34:48 +0700

    change .env file

--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
-  const config = new DocumentBuilder()
-    .setTitle('GPLX example')
-    .setDescription('GPLX API description')
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/gplx', app, document);
+  // const config = new DocumentBuilder()
+  //   .setTitle('GPLX example')
+  //   .setDescription('GPLX API description')
+  //   .setVersion('1.0')
+  //   .build();
+  // const document = SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup('api/gplx', app, document);
   await app.listen(3000);
 }
 bootstrap();
